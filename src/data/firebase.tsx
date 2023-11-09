de2c224dbe4776d8476a8d7b662580aba7652ec3
hnht95 <nguyennhathao22503@gmail.com>
2023-11-09 17:24:31 +0700

    second push

--- src/data/firebase.tsx
+++ src/data/firebase.tsx
@@ -1,13 +1,13 @@
-// import { initializeApp } from "firebase/app";
-// // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-// const firebaseConfig = {
-//   apiKey: "AIzaSyBv47Z3xIZw10oFF4JTFrAVVnap8BgnIWU",
-//   authDomain: "charmcharm-e0d9d.firebaseapp.com",
-//   projectId: "charmcharm-e0d9d",
-//   storageBucket: "charmcharm-e0d9d.appspot.com",
-//   messagingSenderId: "946258202522",
-//   appId: "1:946258202522:web:237cf14d1968a3cd473423",
-//   measurementId: "G-0ZF03LBZWL",
-// };
+import { initializeApp } from "firebase/app";
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyBv47Z3xIZw10oFF4JTFrAVVnap8BgnIWU",
+  authDomain: "charmcharm-e0d9d.firebaseapp.com",
+  projectId: "charmcharm-e0d9d",
+  storageBucket: "charmcharm-e0d9d.appspot.com",
+  messagingSenderId: "946258202522",
+  appId: "1:946258202522:web:237cf14d1968a3cd473423",
+  measurementId: "G-0ZF03LBZWL",
+};
 
-// const app = initializeApp(firebaseConfig);
+const app = initializeApp(firebaseConfig);
